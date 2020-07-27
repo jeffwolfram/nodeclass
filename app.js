fcff8b544b0fff38807333ccd0d896ba7c367018
Jeff Wolfram <jeffwolfram@gmail.com>
2020-07-27 08:54:01 -0700

    added 404 route

--- app.js
+++ app.js
@@ -1,21 +1,20 @@
-const http = require('http');
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
 
 const adminRoutes = require('./routes/admin');
 
 const shopRoutes = require('./routes/shop');
 
-const express = require('express');
-
-const app = express();
-
-const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-
-const server = http.createServer(app);
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found</h1>')
+});
 
 app.listen(3000);
