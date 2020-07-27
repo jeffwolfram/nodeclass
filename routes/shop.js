868e2a51ce033106b16550fb74b2cab400ab6488
Jeff Wolfram <jeffwolfram@gmail.com>
2020-07-27 13:40:24 -0700

    added styles to each page

--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,13 @@ const path = require('path');
 
 const express = require('express');
 
+const rootDir = require('../util/path');
+
 const router = express.Router();
 
-
+// rootDir was created in util/path.js  and is created at top of page in const rootdir
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(__dirname,'../', 'views', 'shop.html'));
+  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 
