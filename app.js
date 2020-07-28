2f0e8f94d3f61e7539c4d77c1ff517625cb52afc
Jeff Wolfram <jeffwolfram@gmail.com>
2020-07-28 09:07:27 -0700

    added pug pages to all views and created template for header.

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname,  'views', '404-page.html'));
+    res.status(404).render('404', {pageTitle: '404 Page not found!'})
 });
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -12,7 +12,7 @@ const router = express.Router();
 // rootDir was created in util/path.js  and is created at top of page in const rootdir
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', {prods: products, docTitle: 'My Shop'});
+  res.render('shop', { prods: products, docTitle: 'My Shop', path: '/'});
 });
 
 
