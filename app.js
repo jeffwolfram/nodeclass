95095ed51562be47b6d4d0940a19f6f9755b5110
Jeff Wolfram <jeffwolfram@gmail.com>
2020-07-27 20:12:42 -0700

    shared info from my admin shop add product page to my shop page

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 
 const shopRoutes = require('./routes/shop');
 
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //added part below to connect css file from folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminRoutes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- routes/shop.js
+++ routes/shop.js
@@ -5,10 +5,13 @@ const express = require('express');
 
 const rootDir = require('../util/path');
 
+const adminData = require('./admin');
+
 const router = express.Router();
 
 // rootDir was created in util/path.js  and is created at top of page in const rootdir
 router.get('/', (req, res, next) => {
+  console.log(adminData.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
