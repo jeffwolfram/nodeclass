77f345c9554fe4f6bc678c5372d8a9c58d8d7887
Jeff Wolfram <jeffwolfram@gmail.com>
2020-07-29 10:53:21 -0700

    refactored code in the model

--- models/product.js
+++ models/product.js
@@ -1,5 +1,22 @@
 const fs = require('fs');
 const path = require('path');
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = (cb) => {
+  
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+       cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+    
+  });
+};
 
 
 
@@ -11,35 +28,20 @@ module.exports = class Product {
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-     'data',
-      'products.json' 
-    );
-    fs.readFile(p,(err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
+    fs.readFile(p,(err, fileContent) => {
+     
+      
+    });
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p,(err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
      
   }
 
